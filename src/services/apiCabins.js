9cc4b4d0eb3cb84aa78ee2dc31c7009c02a49c16
Ehab Hussein <ehabsmh3@gmail.com>
2025-01-21 17:18:41 +0200

    delete cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -6,5 +6,16 @@ export async function getCabins() {
     console.error(error);
     throw new Error("Cabins could not be loaded.");
   }
+
   return cabins;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("Cabin could not be deleted.");
+  }
+
+  return data;
 }
